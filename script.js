f45e529ac5d011df138a982dac973c8796b04d78
Arbaj Ansari <83424578+ArbajAr7@users.noreply.github.com>
2024-05-14 15:54:20 +0545

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+function GetLevel(element) {
+	let level = 0;
+	let current_element = element;
+	if(current_element.parentNode)
+	{
+		level +=1;
+		current_element = current_element.parentNode;
+	}
+
+	return level;
+}
+
+let my_element = document.getElementById.("level");
+const level = GetLevel(my_element);
+alert("The level of the element is: "+level);
